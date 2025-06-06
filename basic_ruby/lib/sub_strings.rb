b16e7bfdfc05725d29665e6282bee07f03cb1f5d
Lucas Figueiredo <lucas.fig50@gmail.com>
2025-06-06 10:25:36 -0300

    Refactoring '/basic_ruby', remove static files

--- basic_ruby/lib/sub_strings.rb
+++ basic_ruby/lib/sub_strings.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
-dictionary = %w[ass come shit wow hey what man goes on]
 
-def substrings(input_text, list)
-  output = Hash.new(0)
-
-  list.each do |dic_text|
-    verified = input_text.scan(dic_text.downcase)
-    output[dic_text] = verified.length if verified.length.positive?
+class SubString
+  def initialize(input_text, list = [])
+    @input = input_text
+    @dictionary = list
   end
+  
+  def start
+    output = Hash.new(0)
 
-  output
+    return 'none' if @input.length == 0 || @dictionary.length == 0
+    
+    @dictionary.each do |dic_text|
+      verified = @input.scan(dic_text.downcase)
+      output[dic_text] = verified.length if verified.length.positive?
+    end
+
+    return output
+  end
 end
-
-puts substrings('Lucas e Lucas onwowwow', dictionary)
